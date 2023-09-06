d57d915552466806a79c6622c0b43936eb737ff8
“Sasha-Coding-Bertolini” <bertolinisasha@gmail.com>
2023-09-06 17:13:56 +0200

    change something in main.yml

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 # Assign a function to be called when the path `/` is requested
 @app.route('/')
 def index():
-    return 'Change this  line'
+    return 'Change   line'
 
 @app.route('/cow')
 def cow():
