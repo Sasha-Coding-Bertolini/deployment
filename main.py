a9e1a7fde7811ad5dcd3e0f8609ab200ebec867e
“Sasha-Coding-Bertolini” <bertolinisasha@gmail.com>
2023-09-07 12:05:44 +0200

    changes made

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 # Assign a function to be called when the path `/` is requested
 @app.route('/')
 def index():
-    return 'Sasha has made'
+    return 'Sasha  made'
 
 @app.route('/cow')
 def cow():
